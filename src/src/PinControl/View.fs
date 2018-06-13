db255a0aef3816c6eb92f9fa00e351b85820ad60
Mikhail Kilianovski <mitutee@gmail.com>
2018-06-13 09:18:30 +0300

    Add styling to pins

--- src/src/PinControl/View.fs
+++ src/src/PinControl/View.fs
@@ -4,22 +4,26 @@ open Fable.Core
 open Fable.Helpers.React
 open Fable.Helpers.React.Props
 open Types
-let simpleButton txt action dispatch =
+let simpleButton txt action dispatch extClass =
     div
       [ ClassName "column is-narrow" ]
       [ a
-          [ ClassName "button"
+          [ ClassName ("button " + extClass )
             OnClick (fun _ -> action |> dispatch) ]
           [ str txt ] ]
 
 let root (model:Model) dispatch =
     div
-        [ ClassName "columns is-vcentered" ]
-        [ div
-            [ ClassName "column is-half is-offset-one-quarter"
-              Style
-                [ CSSProp.Width "170px" ] ]
-            [ ]
-          simpleButton "TURN ON" TurnOn dispatch
-          simpleButton "TURN OFF" TurnOff dispatch
+        [ ]
+        [
+          h1
+            [ ClassName "title" ]
+            [str (sprintf "Pin #%i"model.PinId) ] 
+          div 
+            [ ClassName "columns"]
+            [
+              simpleButton "TURN ON" TurnOn dispatch "is-info"
+              simpleButton "TURN OFF" TurnOff dispatch "is-danger"
+            ]
+          hr []         
         ]
